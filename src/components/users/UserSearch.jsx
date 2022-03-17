fa0aad9d3d0d0e8d44bac8c5bb28edd863df8048
Matheus Trinca <matheusfelipetrinca@gmail.com>
2022-03-17 03:15:38 -0300

    button

--- src/components/users/UserSearch.jsx
+++ src/components/users/UserSearch.jsx
@@ -47,7 +47,10 @@ const UserSearch = () => {
       </div>
       {users.length > 0 && (
         <div>
-          <button onClick={clearUsers} className="btn btn-ghost btn-lg">
+          <button
+            onClick={clearUsers}
+            className="md:align-top btn btn-ghost btn-lg"
+          >
             Clear
           </button>
         </div>
